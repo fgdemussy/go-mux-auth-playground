e9d9c4b57f96808228e699681547afef016c4dc2
François Gueneau de Mussy <fgdemussy@gmail.com>
2020-12-18 18:34:16 -0300

    feat: basic auth

--- main.go
+++ main.go
@@ -10,11 +10,27 @@ import (
 func main() {
 	r := mux.NewRouter()
 	r.HandleFunc("/", SomeHandler)
+	r.Use(basicAuthMiddleware)
 	log.Fatal(http.ListenAndServe(":8000", r))
 }
 
 // SomeHandler does this
 func SomeHandler(w http.ResponseWriter, r *http.Request) {
 	w.WriteHeader(http.StatusOK)
-	w.Write([]byte("Gorilla!\n"))
+	w.Write([]byte("handled OK!\n"))
+}
+
+func basicAuthMiddleware(next http.Handler) http.Handler  {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Set("www-Authenticate", `Basic realm="Restricted"`)
+
+		username, password, _ := r.BasicAuth()
+
+		if username != "fran" || password != "pass" {
+			http.Error(w, "Not Authorized", http.StatusUnauthorized)
+			return
+		}
+
+		next.ServeHTTP(w, r)
+	})
 }
